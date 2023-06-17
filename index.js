7a6253e70bdc0125c2ce3788641d8c481af509f3
taetaetae <taetaetae_@naver.com>
2023-06-18 00:01:27 +0900

    output test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  core.setOutput("time", "hello");
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
